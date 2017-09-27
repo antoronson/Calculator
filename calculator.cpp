e7a58236f7ce130237c87fdcb1589ac200a67b17
antoronson <antoronson590@gmail.com>
2017-09-27 14:24:41 +0200

    Updated with Shunt Yard Algorithm

--- calculator.cpp
+++ calculator.cpp
@@ -5,12 +5,13 @@
 #include <cstdlib>
 #include <cmath>
 #include <sstream>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
 double Calculator::separator(std::string inum)
 {
-	//c-count
+/*	//c-count
 	int c_inum = 0, c_sym = 0;
 	for(int i = 0; i < inum.size(); i++)	// Read chracter by character and count number of symbols
 	{
@@ -36,9 +37,9 @@ double Calculator::separator(std::string inum)
 	value = new double[c_sym + 1];			// The value storage
 	syms = new double[c_sym];				// Symbol Storage + -> 2, - -> 1, * -> 4, / -> 3 Order or Priority
 	int c_c_sym = 0, c_c_num = 0;
-	double temp_s_i = 0;
+*/	// Two Stacks, Two Bool Variables, Two Temporary Storage Variable. 
 	std::stringstream temp;					// Temporary memory to convert string to integer
-
+	double temp_s_i = 0, num_accu = 0, syms = 0, last_op = 0, this_op = 0;
 	for(int i = 0; i < inum.size(); i++)// For every character read, we generate number and symbol array
 	{
 		if (isdigit(inum[i]))
@@ -46,92 +47,143 @@ double Calculator::separator(std::string inum)
 			temp <<inum[i];
 			temp >> temp_s_i;
 			temp.clear();
-			value[c_c_num] = value[c_c_num]*10 + temp_s_i;
+			num_accu = num_accu*10 + temp_s_i;	
 		}
 		else
 		{
-			c_c_num++;
+		numbers.push(num_accu);
+		num_accu = 0;
 		switch (inum[i])
 		{ 
-		case '+':
-			syms[c_c_sym] = 2;
-			break;
-		case '-':
-			syms[c_c_sym] = 1;
-			break;
-		case '*':
-			syms[c_c_sym] = 4;
-			break;
-		case '/':
-			syms[c_c_sym] = 3;
-			break;
-		default:
-			syms[c_c_sym] = -1;
+			case '+':
+				syms = -2;
+				break;
+			case '-':
+				syms = -1;
+				break;
+			case '*':
+				syms = -4;
+				break;
+			case '/':
+				syms = -3;
+				break;
+			default:
+				syms = 0;
 		}
-		
-
-		c_c_sym++;
+		operators.push(syms);
 		}
-	}
 
-	for(int i =0; i < c_sym; i++)
-
-		cout<<syms[i]<<endl;
-	cout<<"--------------------------------------------"<<endl;
-	for(int i = 0; i<c_sym+1;i++)
-		cout<<value[i]<<endl;
-}
-
-double Calculator::calculaiton()
-{
-
-	// Local Variable Initialization
-	// Absorbs value and syms variable from class library
-
-	int ord_ = 4, sym_size = 0;
-	double ans =0;
-
-	sym_size = syms.size();
-
-	// Find the max order of precedence
-
-	while ord_ ~= 0 
-	{
-		for (int i = 0; i < sym_size; i++)
+		if (!operators.empty())
 		{
-			if(syms[i] == ord_)
+			syms = operators.top();
+			operators.pop();
+			if ((!operators.empty()) && (operators.top() < syms))
 			{
-				double temp_ans =0;
-				switch syms[i]
+				while (!operators.empty())
 				{
-					case 1:
-					{	temp_ans = value[i] - value[i+1];
-						value[i] = temp_ans;
-						value[i+1] = temp_ans;
-						break;
-					}
-					case 2:
-					{
-						temp_ans = value[i] + value[i+1];
-						break;
-					}
+					numbers.push(operators.top());
 
-					case 3:
-					{
-						temp_ans = value[i] / value[i+1];
-						break;
-					}
-
-					case 4:
-					{
-						temp_ans = value[i] * value[i+1];
-						break;
-					}
-					default:
-						cout<<"Error"<<endl;
+					operators.pop();
 				}
-				value[i] = temp
+				operators.push(syms);
+		
+			}
+			else
+			{
+				operators.push(syms);
 			}
 		}
-	} 
-}
+
+	}
+	numbers.push(num_accu);
+	while (!operators.empty())
+	{
+		numbers.push(operators.top());
+		operators.pop();
+	}
+
+};
+
+
+double Calculator::calculation()
+{
+	// We reorganize the stack and empty the numbers stack to operators stack
+	// Variable Initialization
+
+	double op1 = 0, op2 = 0, ope = 0, temp_ans = 0;
+	bool flag = false;
+	
+	// Push from numbers to operators
+
+	while (!numbers.empty())
+	{
+		operators.push(numbers.top());
+		numbers.pop();
+	}
+
+
+	while (!operators.empty())
+	{
+		while ((!operators.empty()) && (operators.top() >0))
+		{
+			numbers.push(operators.top());
+			operators.pop();
+		}
+
+		op2 = numbers.top();
+		numbers.pop();
+		if (!numbers.empty())
+		{
+			op1 = numbers.top();
+			numbers.pop();
+		}
+		else
+		{
+			flag = true;
+			cout<<"Answer is" << op2<<endl;
+			// Declare op2 as output and break the loop break the loop
+		}
+		if (flag == true) 
+			break;
+		ope = operators.top();
+		operators.pop();
+		cout<<"No Error"<<endl;
+		cout<<op1<<op2<<ope<<endl;
+		temp_ans = calculations(op1, op2, ope);
+		operators.push(temp_ans);
+		while (!numbers.empty())
+		{
+			operators.push(numbers.top());
+			numbers.pop();
+		}
+		if (flag == true)
+			break;
+	}
+
+};
+
+double Calculator::calculations (double oper1, double oper2, double operat)
+{
+	double local_ans = 0;
+	int operation = static_cast<int>(operat);
+		cout<<operation<<endl;
+		switch (operation)
+		{ 
+			case -2:
+				local_ans = oper1 + oper2;
+				break;
+			case -1:
+				local_ans = oper1 - oper2;
+				break;
+			case -4:
+				local_ans = oper1 * oper2;
+				break;
+			case -3:
+				local_ans = oper1 / oper2;
+				break;
+			default:
+				cout<<"Invalid ! Error in calculation"<<endl;
+		}
+		cout<<local_ans<<endl;
+		return local_ans;
+};
--- calculator.h
+++ calculator.h
@@ -3,16 +3,20 @@
 
 #include <cstdlib>
 #include <string>
+#include <stack>
+
 
 class Calculator
 {
 private:
-	double *value, *syms;	// The Value array and corresponding symbols array generated
+	std::stack<double> numbers;		// Number storage Stack - Global
+	std::stack<double> operators;			// Operator Storage - Global
 	std::string inum;			// The input expression
 
 public:
 	double separator(std::string inum);
 	double calculation();
+	double calculations(double oper1, double oper2, double operat);
 
 };
 #endif
